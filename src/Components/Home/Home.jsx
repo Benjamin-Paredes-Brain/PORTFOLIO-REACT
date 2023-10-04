100c41da9e76890fcb03f27792bb722f2c69a8a7
Benjamin Paredes Brain <benja2005pb@gmail.com>
2023-10-03 23:14:28 -0300

    about home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,9 +15,9 @@ export const Home = () => {
 
 
                 <div className="about_container">
-                    <p className="about">Hi, I'm Benjamin Martin Paredes Brain, a programming enthusiast who embarked on this exciting journey in the year 2022. Since then, I've cultivated a deep admiration for the art of coding and immerse myself in a constant learning process.</p>
-                    <p className="about">My current focus revolves around crafting high-quality web applications and continuously advancing my professional skills. My aim is to push my own boundaries every day, offering increasingly innovative and efficient solutions to the technological challenges we encounter.</p>
-                    <p className="about">Outside the realm of programming, I relish diving into the pages of books, exploring new worlds through reading, and nurturing my mind. Additionally, I'm passionate about well-being and health, and you'll regularly find me at the gym, working on both my body and mind.</p>
+                    <p className="about">Hi, I'm Benjamin Martin Paredes Brain, a programming enthusiast who embarked on this exciting journey in the year 2022. Since then, I have a deep admiration for this coding and programming art, also I consider myself as a person committed to constant learning.</p>
+                    <p className="about">My current focus revolves around craft high-quality web applications and continuously improving my professional skills. My goal is to push my own every day, offering increasingly innovative and efficient results to technological challenges we confront.</p>
+                    <p className="about">In the other hand, I enjoy nurturing my mind through books, exploring new worlds by reading and acquiring knowledge. In addition, I'm passionate about well-being and health. You'll regularly find me at the gym, working on my mind and body.</p>
                 </div>
 
 
